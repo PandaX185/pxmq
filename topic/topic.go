5204c006020752042e83cb806c4092724286c812
PandaX185 <mouss3abbb@gmail.com>
2026-01-21 17:21:47 +0200

    Refactor topic handling to replace WAL with segment-based storage and implement segment rotation

--- topic/topic.go
+++ topic/topic.go
@@ -9,39 +9,45 @@ import (
 	"path/filepath"
 	"pxmq/client"
 	"pxmq/message"
+	"regexp"
+	"sort"
+	"strconv"
 	"sync"
 	"time"
 )
 
 type Topic struct {
-	Name        string
-	messages    []message.Message
-	subscribers map[*client.Subscriber]bool
-	mu          sync.RWMutex
-	cv          *sync.Cond
-	walFile     *os.File
-	nextMsgID   uint64
-	walPath     string
+	Name            string
+	messages        []message.Message
+	subscribers     map[*client.Subscriber]bool
+	mu              sync.RWMutex
+	cv              *sync.Cond
+	activeSegment   *os.File
+	nextMsgID       uint64
+	dataDir         string
+	activeSegmentID uint64
+	maxSegmentSize  int64
 }
 
 func NewTopic(name string, dataDir string) *Topic {
-	walPath := filepath.Join(dataDir, name+".wal")
 	t := &Topic{
-		Name:        name,
-		messages:    make([]message.Message, 0),
-		subscribers: make(map[*client.Subscriber]bool),
-		walPath:     walPath,
-		nextMsgID:   1,
+		Name:           name,
+		messages:       make([]message.Message, 0),
+		subscribers:    make(map[*client.Subscriber]bool),
+		dataDir:        dataDir,
+		nextMsgID:      1,
+		maxSegmentSize: 64 * 1024 * 1024,
 	}
 	t.cv = sync.NewCond(&t.mu)
 
-	t.loadWAL()
-
-	var err error
-	t.walFile, err = os.OpenFile(walPath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
-	if err != nil {
-		panic(fmt.Sprintf("Failed to open WAL file for topic %s: %v", name, err))
+	topicDir := filepath.Join(dataDir, name)
+	if err := os.MkdirAll(topicDir, 0755); err != nil {
+		panic(fmt.Sprintf("Failed to create topic directory for %s: %v", name, err))
 	}
+
+	t.loadSegments()
+
+	t.openActiveSegment()
 
 	go func() {
 		ticker := time.NewTicker(60 * 60 * time.Second)
@@ -64,7 +70,7 @@ func (t *Topic) Publish(payload []byte) {
 	msg := message.NewMessage(t.nextMsgID, payload)
 	t.nextMsgID++
 
-	if err := t.appendToWAL(msg); err != nil {
+	if err := t.appendToActiveSegment(msg); err != nil {
 		panic(fmt.Sprintf("Failed to append to WAL: %v", err))
 	}
 
@@ -132,13 +138,42 @@ func (t *Topic) HasSubscriber(sub *client.Subscriber) bool {
 	return exists
 }
 
-func (t *Topic) loadWAL() {
-	file, err := os.Open(t.walPath)
+func (t *Topic) loadSegments() {
+	topicDir := filepath.Join(t.dataDir, t.Name)
+	pattern := filepath.Join(topicDir, "segment_*")
+	matches, err := filepath.Glob(pattern)
 	if err != nil {
-		if os.IsNotExist(err) {
-			return
+		panic(fmt.Sprintf("Failed to glob segment files for topic %s: %v", t.Name, err))
+	}
+
+	if len(matches) == 0 {
+		t.activeSegmentID = 1
+		return
+	}
+
+	var segmentIDs []uint64
+	segmentRegex := regexp.MustCompile(`segment_(\d+)$`)
+	for _, match := range matches {
+		if matches := segmentRegex.FindStringSubmatch(filepath.Base(match)); len(matches) == 2 {
+			if id, err := strconv.ParseUint(matches[1], 10, 64); err == nil {
+				segmentIDs = append(segmentIDs, id)
+			}
 		}
-		panic(fmt.Sprintf("Failed to open WAL file for topic %s: %v", t.Name, err))
+	}
+	sort.Slice(segmentIDs, func(i, j int) bool { return segmentIDs[i] < segmentIDs[j] })
+
+	for _, segmentID := range segmentIDs {
+		segmentPath := filepath.Join(topicDir, fmt.Sprintf("segment_%d", segmentID))
+		t.loadSegment(segmentPath)
+	}
+
+	t.activeSegmentID = segmentIDs[len(segmentIDs)-1]
+}
+
+func (t *Topic) loadSegment(segmentPath string) {
+	file, err := os.Open(segmentPath)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to open segment file %s: %v", segmentPath, err))
 	}
 	defer file.Close()
 
@@ -149,17 +184,17 @@ func (t *Topic) loadWAL() {
 			if err == io.EOF {
 				break
 			}
-			panic(fmt.Sprintf("Failed to read msgID from WAL for topic %s: %v", t.Name, err))
+			panic(fmt.Sprintf("Failed to read msgID from segment %s: %v", segmentPath, err))
 		}
 
 		var payloadLen uint32
 		if err := binary.Read(reader, binary.BigEndian, &payloadLen); err != nil {
-			panic(fmt.Sprintf("Failed to read payload length from WAL for topic %s: %v", t.Name, err))
+			panic(fmt.Sprintf("Failed to read payload length from segment %s: %v", segmentPath, err))
 		}
 
 		payload := make([]byte, payloadLen)
 		if _, err := io.ReadFull(reader, payload); err != nil {
-			panic(fmt.Sprintf("Failed to read payload from WAL for topic %s: %v", t.Name, err))
+			panic(fmt.Sprintf("Failed to read payload from segment %s: %v", segmentPath, err))
 		}
 
 		msg := message.NewMessage(msgID, payload)
@@ -171,23 +206,66 @@ func (t *Topic) loadWAL() {
 	}
 }
 
-func (t *Topic) appendToWAL(msg *message.Message) error {
-	if err := binary.Write(t.walFile, binary.BigEndian, msg.ID); err != nil {
+func (t *Topic) openActiveSegment() {
+	topicDir := filepath.Join(t.dataDir, t.Name)
+	segmentPath := filepath.Join(topicDir, fmt.Sprintf("segment_%d", t.activeSegmentID))
+	var err error
+	t.activeSegment, err = os.OpenFile(segmentPath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to open active segment for topic %s: %v", t.Name, err))
+	}
+}
+
+func (t *Topic) appendToActiveSegment(msg *message.Message) error {
+	if t.shouldRotateSegment() {
+		if err := t.rotateSegment(); err != nil {
+			return fmt.Errorf("failed to rotate segment: %w", err)
+		}
+	}
+
+	if err := binary.Write(t.activeSegment, binary.BigEndian, msg.ID); err != nil {
 		return err
 	}
 	payloadLen := uint32(len(msg.Payload))
-	if err := binary.Write(t.walFile, binary.BigEndian, payloadLen); err != nil {
+	if err := binary.Write(t.activeSegment, binary.BigEndian, payloadLen); err != nil {
 		return err
 	}
-	if _, err := t.walFile.Write(msg.Payload); err != nil {
+	if _, err := t.activeSegment.Write(msg.Payload); err != nil {
 		return err
 	}
-	return t.walFile.Sync()
+	return t.activeSegment.Sync()
+}
+
+func (t *Topic) shouldRotateSegment() bool {
+	if t.activeSegment == nil {
+		return false
+	}
+
+	stat, err := t.activeSegment.Stat()
+	if err != nil {
+		return false
+	}
+
+	return stat.Size() >= t.maxSegmentSize
+}
+
+func (t *Topic) rotateSegment() error {
+	if t.activeSegment != nil {
+		if err := t.activeSegment.Close(); err != nil {
+			return err
+		}
+	}
+
+	t.activeSegmentID++
+
+	t.openActiveSegment()
+
+	return nil
 }
 
 func (t *Topic) Close() error {
-	if t.walFile != nil {
-		return t.walFile.Close()
+	if t.activeSegment != nil {
+		return t.activeSegment.Close()
 	}
 	return nil
 }
@@ -222,42 +300,35 @@ func (t *Topic) Prune() error {
 		keepIndex = len(t.messages)
 	}
 
-	tmpFile := t.walPath + ".tmp"
-	tmp, err := os.Create(tmpFile)
+	if keepIndex == 0 {
+		return nil
+	}
+
+	if t.activeSegment != nil {
+		t.activeSegment.Close()
+		t.activeSegment = nil
+	}
+
+	topicDir := filepath.Join(t.dataDir, t.Name)
+	pattern := filepath.Join(topicDir, "segment_*")
+	matches, err := filepath.Glob(pattern)
 	if err != nil {
 		return err
 	}
-	defer tmp.Close()
+	for _, match := range matches {
+		if err := os.Remove(match); err != nil {
+			return err
+		}
+	}
+
+	t.activeSegmentID = 1
+	t.openActiveSegment()
 
 	for i := keepIndex; i < len(t.messages); i++ {
 		msg := &t.messages[i]
-		if err := binary.Write(tmp, binary.BigEndian, msg.ID); err != nil {
+		if err := t.appendToActiveSegment(msg); err != nil {
 			return err
 		}
-		payloadLen := uint32(len(msg.Payload))
-		if err := binary.Write(tmp, binary.BigEndian, payloadLen); err != nil {
-			return err
-		}
-		if _, err := tmp.Write(msg.Payload); err != nil {
-			return err
-		}
-	}
-
-	if err := tmp.Sync(); err != nil {
-		return err
-	}
-
-	if t.walFile != nil {
-		t.walFile.Close()
-	}
-
-	if err := os.Rename(tmpFile, t.walPath); err != nil {
-		return err
-	}
-
-	t.walFile, err = os.OpenFile(t.walPath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
-	if err != nil {
-		return err
 	}
 
 	t.messages = t.messages[keepIndex:]
